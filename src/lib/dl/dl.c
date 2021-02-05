76f9caea44cfe045bd465cd4097c56efb4450cde
Taxerap <taxerap@gmail.com>
2021-02-05 02:03:24 -0600

    Add dl loading

---
+++ src/lib/dl/dl.c
@@ -0,0 +1,67 @@
+/*************************************************************************************************
+*                                                                                                *
+*                                  [ State of War: Remastered ]                                  *
+*                                                                                                *
+**************************************************************************************************
+*                                                                                                *
+*                  A free, open-source software project recreating an old game.                  *
+*               (c) 2017 - 2021 State of War Baidu Postbar, some rights reserved.                *
+*                                                                                                *
+*    State of War: Remastered is a free software. You can freely do whatever you want with it    *
+*     under the JUST DON'T BOTHER ME PUBLIC LICENSE (hereinafter referred to as the license)     *
+*                                   published by mhtvsSFrpHdE.                                   *
+*                                                                                                *
+*  By the time this line is written, the version of the license document is 1, but you may use   *
+* any later version of the document released by mhtvsSFrpHdE <https://github.com/mhtvsSFrpHdE>.  *
+*                                                                                                *
+*     State of War: Remastered is created, intended to be useful, but without any warranty.      *
+*                      For more information, please forward to the license.                      *
+*                                                                                                *
+*       You should have received a copy of the license along with the source code of this        *
+*  program. If not, please see https://github.com/mhtvsSFrpHdE/ipcui/blob/master/LICENSE_JDBM.   *
+*                                                                                                *
+*      For more information about the project and us, please visit our Github repository at      *
+*                         https://github.com/State-of-War-PostBar/sowr.                          *
+*                                                                                                *
+*                               Mission is successfully completed.                               *
+*                                                                                                *
+**************************************************************************************************/
+
+#include "dl.h"
+
+#include "../container/vector.h"
+#include "../data/unicode.h"
+
+sowr_DLib
+sowr_LoadDynamicLibrary(const char *path)
+{
+#ifdef SOWR_TARGET_WINDOWS
+    sowr_Vector utf16 = sowr_Vector_CreateS(sizeof(unsigned char), NULL);
+    sowr_Unicode_UTF8ToUTF16((unsigned char *) path, &utf16);
+    sowr_DLib dl = LoadLibraryW(utf16.ptr);
+    sowr_Vector_DestroyS(&utf16);
+    return dl;
+#else
+    return dlopen(path, RTLD_LAZY);
+#endif
+}
+
+sowr_ExFunc
+sowr_GetDLSymbol(sowr_DLib lib, const char *sym)
+{
+#ifdef SOWR_TARGET_WINDOWS
+    return GetProcAddress(lib, sym);
+#else
+    return dlsym(lib, sym);
+#endif
+}
+
+int
+sowr_FreeDynamicLibrary(sowr_DLib lib)
+{
+#ifdef SOWR_TARGET_WINDOWS
+    return FreeLibrary(lib);
+#else
+    return dlclose(lib);
+#endif
+}
---
+++ src/lib/dl/dl.h
@@ -0,0 +1,86 @@
+/*************************************************************************************************
+*                                                                                                *
+*                                  [ State of War: Remastered ]                                  *
+*                                                                                                *
+**************************************************************************************************
+*                                                                                                *
+*                  A free, open-source software project recreating an old game.                  *
+*               (c) 2017 - 2021 State of War Baidu Postbar, some rights reserved.                *
+*                                                                                                *
+*    State of War: Remastered is a free software. You can freely do whatever you want with it    *
+*     under the JUST DON'T BOTHER ME PUBLIC LICENSE (hereinafter referred to as the license)     *
+*                                   published by mhtvsSFrpHdE.                                   *
+*                                                                                                *
+*  By the time this line is written, the version of the license document is 1, but you may use   *
+* any later version of the document released by mhtvsSFrpHdE <https://github.com/mhtvsSFrpHdE>.  *
+*                                                                                                *
+*     State of War: Remastered is created, intended to be useful, but without any warranty.      *
+*                      For more information, please forward to the license.                      *
+*                                                                                                *
+*       You should have received a copy of the license along with the source code of this        *
+*  program. If not, please see https://github.com/mhtvsSFrpHdE/ipcui/blob/master/LICENSE_JDBM.   *
+*                                                                                                *
+*      For more information about the project and us, please visit our Github repository at      *
+*                         https://github.com/State-of-War-PostBar/sowr.                          *
+*                                                                                                *
+*                               Mission is successfully completed.                               *
+*                                                                                                *
+**************************************************************************************************/
+
+#ifndef SOWR_LIB_DL_DL_H
+#define SOWR_LIB_DL_DL_H
+
+#include <pch.h>
+
+#ifdef SOWR_TARGET_WINDOWS
+    typedef HMODULE sowr_DLib;
+    typedef FARPROC sowr_ExFunc;
+
+    #define SOWR_CURRENT_PROC NULL
+#else
+    typedef void *sowr_DLib;
+    typedef void *sowr_ExFunc;
+
+    #define SOWR_CURRENT_PROC RTLD_DEFAULT
+#endif
+
+///
+/// \brief Load a library
+///
+/// Load a dynamic library.
+///
+/// \param path Path to the library file
+///
+/// \return Handle of the library, or NULL if errors occur
+///
+sowr_DLib
+sowr_LoadDynamicLibrary(const char *path);
+
+///
+/// \brief Get a process symbol of the library
+///
+/// Get a process symbol inside a dynamic library.
+///
+/// \param lib Handle of the library. Use \a SOWR_CURRENT_PROC for current process
+/// \param sym Symbol
+///
+/// \note NULL returned by this function does not necessarily indicates an error.
+///
+/// \return Retrieved symbol
+///
+sowr_ExFunc
+sowr_GetDLSymbol(sowr_DLib lib, const char *sym);
+
+///
+/// \brief Free a library
+///
+/// Free a dynamic library.
+///
+/// \param lib Handle of the library
+///
+/// \return 0 if success
+///
+int
+sowr_FreeDynamicLibrary(sowr_DLib lib);
+
+#endif // !SOWR_LIB_DL_DL_H
