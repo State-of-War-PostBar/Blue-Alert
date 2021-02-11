4777fb6d913abc644b305e2733ac1b26c8980320
Taxerap <taxerap@gmail.com>
2021-02-11 17:08:34 -0600

    Add thread lib
    I always think WinThread and POSIX threads API are too prolix. A thread executes a function and that's it. Well, exit code could be considered, but not necessary by us.

---
+++ src/lib/sync/thread.c
@@ -0,0 +1,109 @@
+/*************************************************************************************************
+*                                                                                                *
+*                                  [ State of War: Remastered ]                                  *
+*                                                                                                *
+**************************************************************************************************
+*                                                                                                *
+*                  A free, open-source software project recreating an old game.                  *
+*               (c) 2017 - 2021 State of War Baidu Postbar, some rights reserved.                *
+*                                                                                                *
+*    State of War: Remastered is a free software. You can freely do whatever you want with it    *
+*     under the JUST DON'T BOTHER ME PUBLIC LICENSE (hereinafter referred to as the license)     *
+*                                   published by mhtvsSFrpHdE.                                   *
+*                                                                                                *
+*  By the time this line is written, the version of the license document is 1, but you may use   *
+* any later version of the document released by mhtvsSFrpHdE <https://github.com/mhtvsSFrpHdE>.  *
+*                                                                                                *
+*     State of War: Remastered is created, intended to be useful, but without any warranty.      *
+*                      For more information, please forward to the license.                      *
+*                                                                                                *
+*       You should have received a copy of the license along with the source code of this        *
+*  program. If not, please see https://github.com/mhtvsSFrpHdE/ipcui/blob/master/LICENSE_JDBM.   *
+*                                                                                                *
+*      For more information about the project and us, please visit our Github repository at      *
+*                         https://github.com/State-of-War-PostBar/sowr.                          *
+*                                                                                                *
+*                               Mission is successfully completed.                               *
+*                                                                                                *
+**************************************************************************************************/
+
+#include "thread.h"
+
+int
+sowr_Thread_Create( sowr_Thread *thr, sowr_ThreadFunc func, void *arg )
+{
+#ifdef SOWR_TARGET_WINDOWS
+    sowr_Thread thrd = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE) func, arg, 0, NULL);
+    if (!thrd)
+    {
+        *thr = NULL;
+        return -1;
+    }
+    *thr = thrd;
+    return 0;
+#else
+    return pthread_create(thr, NULL, func, arg);
+#endif
+}
+
+sowr_Thread
+sowr_Thread_Current( void )
+{
+#ifdef SOWR_TARGET_WINDOWS
+    return GetCurrentThread();
+#else
+    return pthread_self();
+#endif
+}
+
+void
+sowr_Thread_Sleep( const struct timespec *duration )
+{
+#ifdef SOWR_TARGET_WINDOWS
+    Sleep(duration->tv_sec * 1000.0 + duration->tv_nsec / 0.000001);
+#else
+    sleep(duration);
+#endif
+}
+
+void
+sowr_Thread_Yield( void )
+{
+#ifdef SOWR_TARGET_WINDOWS
+    SwitchToThread();
+#else
+    pthread_yield();
+#endif
+}
+
+noreturn
+void
+sowr_Thread_Exit( void )
+{
+#ifdef SOWR_TARGET_WINDOWS
+    ExitThread(0);
+#else
+    pthread_exit(NULL);
+#endif
+}
+
+void
+sowr_Thread_Detach( sowr_Thread thr )
+{
+#ifdef SOWR_TARGET_WINDOWS
+    CloseHandle(thr);
+#else
+    pthread_detach(thr);
+#endif
+}
+
+void
+sowr_Thread_Join( sowr_Thread thr )
+{
+#ifdef SOWR_TARGET_WINDOWS
+    WaitForSingleObject(thr, INFINITE);
+    CloseHandle(thr);
+#else
+    pthread_join(thrd, &res);
+#endif
+}
---
+++ src/lib/sync/thread.h
@@ -0,0 +1,118 @@
+/*************************************************************************************************
+*                                                                                                *
+*                                  [ State of War: Remastered ]                                  *
+*                                                                                                *
+**************************************************************************************************
+*                                                                                                *
+*                  A free, open-source software project recreating an old game.                  *
+*               (c) 2017 - 2021 State of War Baidu Postbar, some rights reserved.                *
+*                                                                                                *
+*    State of War: Remastered is a free software. You can freely do whatever you want with it    *
+*     under the JUST DON'T BOTHER ME PUBLIC LICENSE (hereinafter referred to as the license)     *
+*                                   published by mhtvsSFrpHdE.                                   *
+*                                                                                                *
+*  By the time this line is written, the version of the license document is 1, but you may use   *
+* any later version of the document released by mhtvsSFrpHdE <https://github.com/mhtvsSFrpHdE>.  *
+*                                                                                                *
+*     State of War: Remastered is created, intended to be useful, but without any warranty.      *
+*                      For more information, please forward to the license.                      *
+*                                                                                                *
+*       You should have received a copy of the license along with the source code of this        *
+*  program. If not, please see https://github.com/mhtvsSFrpHdE/ipcui/blob/master/LICENSE_JDBM.   *
+*                                                                                                *
+*      For more information about the project and us, please visit our Github repository at      *
+*                         https://github.com/State-of-War-PostBar/sowr.                          *
+*                                                                                                *
+*                               Mission is successfully completed.                               *
+*                                                                                                *
+**************************************************************************************************/
+
+#ifndef SOWR_LIB_SYNC_THREAD_H
+#define SOWR_LIB_SYNC_THREAD_H
+
+#include <pch.h>
+
+#ifdef SOWR_TARGET_WINDOWS
+    #include <processthreadsapi.h>
+
+    typedef HANDLE sowr_Thread;
+#else
+    #include <pthread.h>
+
+    typedef pthread_t sowr_Thread;
+#endif
+
+typedef void (*sowr_ThreadFunc)( void * );
+
+///
+/// \brief Create a thread
+///
+/// Create a thread and run it immediately.
+///
+/// \param thr Pointer to a storage of the thread handle
+/// \param func Function to run
+/// \param arg argument to the function
+///
+/// \return 0 if successed
+///
+int
+sowr_Thread_Create( sowr_Thread *thr, sowr_ThreadFunc func, void *arg );
+
+///
+/// \brief Get handle of current thread
+///
+/// Get the handle of the current thread
+///
+/// \return Thread handle
+///
+sowr_Thread
+sowr_Thread_Current( void );
+
+///
+/// \brief Sleep the current thread
+///
+/// Pause the execution of current thread.
+///
+/// \param duration Time to sleep
+///
+void
+sowr_Thread_Sleep( const struct timespec *duration );
+
+///
+/// \brief Yield the time slice
+///
+/// Yield the execution of current thread to others.
+///
+void
+sowr_Thread_Yield( void );
+
+///
+/// \brief Exit the thread
+///
+/// Exit current thread.
+///
+noreturn
+void
+sowr_Thread_Exit( void );
+
+///
+/// \brief Detach a thread
+///
+/// Detach a thread from current thread.
+///
+/// \param thr Thread to detach
+///
+void
+sowr_Thread_Detach( sowr_Thread thr );
+
+///
+/// \brief Join a thread
+///
+/// Block current thread until the thread ends.
+///
+/// \param thr Thread to join
+///
+void
+sowr_Thread_Join( sowr_Thread thr );
+
+#endif // !SOWR_LIB_SYNC_THREAD_H
