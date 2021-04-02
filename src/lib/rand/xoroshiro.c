507865fba487f5d0e6b79c0b1cd052a47de9cc6d
Taxerap <taxerap@gmail.com>
2021-04-01 19:01:24 -0500

    Add prng

---
+++ src/lib/rand/xoroshiro.c
@@ -0,0 +1,77 @@
+/*************************************************************************************************
+*                                                                                                *
+*                                  [ State of War: Remastered ]                                  *
+*                                                                                                *
+**************************************************************************************************
+*                                                                                                *
+*                  A free, open-source software project recreating an old game.                  *
+*               (c) 2017 - 2021 State of War Baidu Postbar, some rights reserved.                *
+*                                                                                                *
+*    State of War: Remastered is a free software. You can freely do whatever you want with it    *
+*     under the JUST DON'T BOTHER ME PUBLIC LICENSE (hereinafter referred to as the license)     *
+*                                   published by mhtvsSFrpHdE.                                   *
+*                                                                                                *
+*  By the time this line is written, the version of the license document is 1, but you may use   *
+* any later version of the document released by mhtvsSFrpHdE <https://github.com/mhtvsSFrpHdE>.  *
+*                                                                                                *
+*     State of War: Remastered is created, intended to be useful, but without any warranty.      *
+*                      For more information, please forward to the license.                      *
+*                                                                                                *
+*       You should have received a copy of the license along with the source code of this        *
+*  program. If not, please see https://github.com/mhtvsSFrpHdE/ipcui/blob/master/LICENSE_JDBM.   *
+*                                                                                                *
+*      For more information about the project and us, please visit our Github repository at      *
+*                         https://github.com/State-of-War-PostBar/sowr.                          *
+*                                                                                                *
+*                               Mission is successfully completed.                               *
+*                                                                                                *
+**************************************************************************************************/
+
+#include "xoroshiro.h"
+
+#include "../data/bytes.h"
+
+static uint64_t sowr_rng_xoroshiro_space[4];
+static const uint64_t SOWR_RNG_XOROSHIRO_SEED_SALT = 0xacbcdc10706ULL;
+
+static inline uint64_t rotate(uint64_t x, int k)
+{
+    return (x << k) | (x >> (64 - k));
+}
+
+void
+sowr_Rng_Xoroshiro_Init( uint64_t seed_hi, uint64_t seed_lo )
+{
+    uint64_t seed_2 = 0ULL, seed_3 = 0ULL;
+    seed_hi ^= SOWR_RNG_XOROSHIRO_SEED_SALT;
+    seed_2 = sowr_SwapEndian64(seed_hi) ^ seed_lo;
+    seed_3 = sowr_SwapEndian64(seed_lo) ^ seed_hi;
+    seed_lo ^= SOWR_RNG_XOROSHIRO_SEED_SALT;
+    sowr_rng_xoroshiro_space[0] = seed_hi;
+    sowr_rng_xoroshiro_space[1] = seed_2;
+    sowr_rng_xoroshiro_space[2] = seed_3;
+    sowr_rng_xoroshiro_space[3] = seed_lo;
+}
+
+uint64_t
+sowr_Rng_Xoroshiro_Next( void )
+{
+    uint64_t rresult = rotate(sowr_rng_xoroshiro_space[1] * 5, 7) * 9;
+    uint64_t temp = sowr_rng_xoroshiro_space[1] << 17;
+
+    sowr_rng_xoroshiro_space[2] ^= sowr_rng_xoroshiro_space[0];
+    sowr_rng_xoroshiro_space[3] ^= sowr_rng_xoroshiro_space[1];
+    sowr_rng_xoroshiro_space[1] ^= sowr_rng_xoroshiro_space[2];
+    sowr_rng_xoroshiro_space[0] ^= sowr_rng_xoroshiro_space[3];
+
+    sowr_rng_xoroshiro_space[2] ^= temp;
+    sowr_rng_xoroshiro_space[3] = rotate(sowr_rng_xoroshiro_space[3], 45);
+
+    return rresult;
+}
+
+uint64_t
+sowr_Rng_Xoroshiro_Ranged( uint64_t limit )
+{
+    return sowr_Rng_Xoroshiro_Next() % limit;
+}
---
+++ src/lib/rand/xoroshiro.h
@@ -0,0 +1,68 @@
+/*************************************************************************************************
+*                                                                                                *
+*                                  [ State of War: Remastered ]                                  *
+*                                                                                                *
+**************************************************************************************************
+*                                                                                                *
+*                  A free, open-source software project recreating an old game.                  *
+*               (c) 2017 - 2021 State of War Baidu Postbar, some rights reserved.                *
+*                                                                                                *
+*    State of War: Remastered is a free software. You can freely do whatever you want with it    *
+*     under the JUST DON'T BOTHER ME PUBLIC LICENSE (hereinafter referred to as the license)     *
+*                                   published by mhtvsSFrpHdE.                                   *
+*                                                                                                *
+*  By the time this line is written, the version of the license document is 1, but you may use   *
+* any later version of the document released by mhtvsSFrpHdE <https://github.com/mhtvsSFrpHdE>.  *
+*                                                                                                *
+*     State of War: Remastered is created, intended to be useful, but without any warranty.      *
+*                      For more information, please forward to the license.                      *
+*                                                                                                *
+*       You should have received a copy of the license along with the source code of this        *
+*  program. If not, please see https://github.com/mhtvsSFrpHdE/ipcui/blob/master/LICENSE_JDBM.   *
+*                                                                                                *
+*      For more information about the project and us, please visit our Github repository at      *
+*                         https://github.com/State-of-War-PostBar/sowr.                          *
+*                                                                                                *
+*                               Mission is successfully completed.                               *
+*                                                                                                *
+**************************************************************************************************/
+
+#ifndef SOWR_LIB_RAND_XOROSHIRO_H
+#define SOWR_LIB_RAND_XOROSHIRO_H
+
+#include <pch.h>
+
+///
+/// \brief Initialize the rng
+///
+/// Initialize the Xoroshiro256** with a 128-bit seed.
+///
+/// \param seed_hi Higher bits of the seed
+/// \param seed_lo Lower bits of the seed
+///
+void
+sowr_Rng_Xoroshiro_Init( uint64_t seed_hi, uint64_t seed_lo );
+
+///
+/// \brief Get next rng
+///
+/// Get next linear sequence. Xoroshiro256** is safe for 2^64 calls.
+///
+/// \return A random number
+///
+uint64_t
+sowr_Rng_Xoroshiro_Next( void );
+
+///
+/// \brief Get a ranged rng
+///
+/// Get a rng in [0, limit). <i>Be wary of % bias.</i>
+///
+/// \param limit Max of the number
+///
+/// \return A random number
+///
+uint64_t
+sowr_Rng_Xoroshiro_Ranged( uint64_t limit );
+
+#endif // !SOWR_LIB_RAND_XOROSHIRO_H
