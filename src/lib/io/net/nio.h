ff077b08bb237dea5501f3a41826752f02178cd5
Taxerap <taxerap@gmail.com>
2021-04-28 22:05:13 -0500

    Preparing

---
+++ src/lib/io/net/nio.h
@@ -0,0 +1,55 @@
+/*************************************************************************************************
+*                                                                                                *
+*                                  [ State of War: Remastered ]                                  *
+*                                                                                                *
+**************************************************************************************************
+*                                                                                                *
+*                  A free, open-source software project recreating an old game.                  *
+*               (c) 2017 - 2021 State of War Baidu Postbar, some rights reserved.                *
+*                                                                                                *
+*    State of War: Remastered is a free software. You can freely do whatever you want with it    *
+*     under the JUST DON'T BOTHER ME PUBLIC LICENSE (hereinafter referred to as the license)     *
+*                                   published by mhtvsSFrpHdE.                                   *
+*                                                                                                *
+*  By the time this line is written, the version of the license document is 1, but you may use   *
+* any later version of the document released by mhtvsSFrpHdE <https://github.com/mhtvsSFrpHdE>.  *
+*                                                                                                *
+*     State of War: Remastered is created, intended to be useful, but without any warranty.      *
+*                      For more information, please forward to the license.                      *
+*                                                                                                *
+*       You should have received a copy of the license along with the source code of this        *
+*  program. If not, please see https://github.com/mhtvsSFrpHdE/ipcui/blob/master/LICENSE_JDBM.   *
+*                                                                                                *
+*      For more information about the project and us, please visit our Github repository at      *
+*                         https://github.com/State-of-War-PostBar/sowr.                          *
+*                                                                                                *
+*                               Mission is successfully completed.                               *
+*                                                                                                *
+**************************************************************************************************/
+
+#ifndef SOWR_LIB_IO_NET_NIO_H
+#define SOWR_LIB_IO_NET_NIO_H
+
+#include <pch.h>
+
+///
+/// Initialize socket
+///
+/// Initialize the socket library (winsock).<br />
+/// <i>On Posix systems, this function does nothing.</i>
+///
+/// \return 0 if success
+///
+int
+sowr_Net_Init( void );
+
+///
+/// Cleanup socket
+///
+/// Cleanup the socket library (winsock).<br />
+/// <i>On Posix systems, this function does nothing.</i>
+///
+void
+sowr_Net_CleanUp( void );
+
+#endif // !SOWR_LIB_IO_NET_NIO_H
